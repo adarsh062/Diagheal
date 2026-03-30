d070d6f00bd1d1f696266de02c76faf66effcb84
adarsh062 <mauryadarsh7492@gmail.com>
2026-03-31 03:31:27 +0530

    build fix 3

--- web_app/src/app/api/auth/[...nextauth]/route.ts
+++ web_app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,15 @@
 export const dynamic = "force-dynamic";
-import { handlers } from "@/auth"
+import { handlers } from "@/auth";
+import { NextRequest } from "next/server";
 
-export const { GET, POST } = handlers
+const isBuildPhase = process.env.NEXT_PHASE === 'phase-production-build';
+
+export const GET = (req: NextRequest) => {
+  if (isBuildPhase) return new Response("OK", { status: 200 });
+  return handlers.GET(req);
+};
+
+export const POST = (req: NextRequest) => {
+  if (isBuildPhase) return new Response("OK", { status: 200 });
+  return handlers.POST(req);
+};
--- web_app/src/auth.ts
+++ web_app/src/auth.ts
@@ -3,8 +3,10 @@ import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma"
 
+const isBuildPhase = process.env.NEXT_PHASE === 'phase-production-build';
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: isBuildPhase ? undefined : PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
 })
