284246c72eaf0c8df73b2a3807a2d07ada7f5cfe
adarsh062 <mauryadarsh7492@gmail.com>
2026-03-30 23:37:45 +0530

    fixed vercel deployment

--- web_app/src/lib/prisma.ts
+++ web_app/src/lib/prisma.ts
@@ -1,18 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL
-  
-  // Initialize connection pool
-  const pool = new Pool({ connectionString })
-  
-  // Create the Prisma PG adapter
-  const adapter = new PrismaPg(pool)
-  
-  // Pass the adapter to PrismaClient
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 declare global {
