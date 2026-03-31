7154f144f45702f457a9cfaa6d382b944dcab206
adarsh062 <mauryadarsh7492@gmail.com>
2026-03-31 21:40:43 +0530

    build fix 5

--- web_app/prisma.config.ts
+++ web_app/prisma.config.ts
@@ -1,12 +1,8 @@
-import { config } from "dotenv";
-config({ path: ".env.local" });
-import { defineConfig } from "@prisma/config";
+import { defineConfig, env } from "@prisma/config";
 
 export default defineConfig({
-  // 'earlyAccess' hata do agar error de raha hai, naye patch mein ye default ho gaya hai
-  schema: "./prisma/schema.prisma", 
+  schema: "prisma/schema.prisma",
   datasource: {
-    // URL ko string confirm karne ke liye ye use karo
-    url: process.env.DATABASE_URL as string,
+    url: env("DATABASE_URL"),
   },
 });
--- web_app/src/lib/prisma.ts
+++ web_app/src/lib/prisma.ts
@@ -1,14 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+// Neon DB connection string
+const connectionString = `${process.env.DATABASE_URL}`;
+
+// Create connection pool and adapter
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+// Pass the adapter to the PrismaClient constructor
 const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
